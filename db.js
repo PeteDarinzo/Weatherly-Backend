bb2846bbcb25037dec9c55b291b90b0fe35ab1a2
Peter Darinzo <birel44@yahoo.com>
2022-03-20 17:37:57 -0500

    restore db.js

--- db.js
+++ db.js
@@ -6,40 +6,40 @@ const { getDatabaseUri } = require("./config");
 let db
 
 /** Springboard Method */
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
-}
-/** END Springboard Method */
-
-
-/** MY METHOD **/
 // if (process.env.NODE_ENV === "production") {
 //   db = new Client({
-//     host: "localhost",
-//     user: "",
-//     password: "",
-//     database: getDatabaseUri(),
+//     connectionString: getDatabaseUri(),
 //     ssl: {
 //       rejectUnauthorized: false
 //     }
 //   });
 // } else {
 //   db = new Client({
-//     host: "localhost",
-//     user: "",
-//     password: "",
-//     database: getDatabaseUri()
+//     connectionString: getDatabaseUri()
 //   });
 // }
+/** END Springboard Method */
+
+
+/** MY METHOD **/
+if (process.env.NODE_ENV === "production") {
+  db = new Client({
+    host: "localhost",
+    user: "pete",
+    password: "vampire",
+    database: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+} else {
+  db = new Client({
+    host: "localhost",
+    user: "pete",
+    password: "vampire",
+    database: getDatabaseUri()
+  });
+}
 /** END MY METHOD **/
 db.connect();
 
