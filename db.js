075f1cda6762440d833fc7ea42cfc501e4486a63
PeteDarinzo <birel44@yahoo.com>
2022-03-31 08:57:17 -0400

    revert db.js to springboard method

--- db.js
+++ db.js
@@ -6,42 +6,42 @@ const { getDatabaseUri } = require("./config");
 let db
 
 /** Springboard Method */
-// if (process.env.NODE_ENV === "production") {
-//   db = new Client({
-//     connectionString: getDatabaseUri(),
-//     ssl: {
-//       rejectUnauthorized: false
-//     }
-//   });
-// } else {
-//   db = new Client({
-//     connectionString: getDatabaseUri()
-//   });
-// }
-/** END Springboard Method */
-
-
-/** MY METHOD **/
-// password must be assigned to Postgresql user using ALTER USER 
-// See: https://chartio.com/resources/tutorials/how-to-set-the-default-user-password-in-postgresql/
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    host: "localhost",
-    user: "pete",
-    password: "vampire",
-    database: getDatabaseUri(),
+    connectionString: getDatabaseUri(),
     ssl: {
       rejectUnauthorized: false
     }
   });
 } else {
   db = new Client({
-    host: "localhost",
-    user: "pete",
-    password: "vampire",
-    database: getDatabaseUri()
+    connectionString: getDatabaseUri()
   });
 }
+/** END Springboard Method */
+
+
+/** MY METHOD **/
+// password must be assigned to Postgresql user using ALTER USER 
+// See: https://chartio.com/resources/tutorials/how-to-set-the-default-user-password-in-postgresql/
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     host: "localhost",
+//     user: "",
+//     password: "",
+//     database: getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
+// } else {
+//   db = new Client({
+//     host: "localhost",
+//     user: "",
+//     password: "",
+//     database: getDatabaseUri()
+//   });
+// }
 /** END MY METHOD **/
 
 db.connect();
