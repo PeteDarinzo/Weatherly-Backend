4d8b21b06a5f376f586e71bca40d009c7d8f0292
PeteDarinzo <birel44@yahoo.com>
2022-04-19 18:08:29 -0400

    redeploy backend

--- db.js
+++ db.js
@@ -3,21 +3,28 @@
 const { Client } = require("pg");
 const { getDatabaseUri } = require("./config");
 
-let db
+const db = new Client({
+  connectionString: getDatabaseUri(),
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+// let db
 
 /** Springboard Method */
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    connectionString: getDatabaseUri(),
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-} else {
-  db = new Client({
-    connectionString: getDatabaseUri()
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     connectionString: getDatabaseUri(),
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+//   });
+// } else {
+//   db = new Client({
+//     connectionString: getDatabaseUri()
+//   });
+// }
 /** END Springboard Method */
 
 
