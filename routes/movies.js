e67cfd6af85036781c0888efca5c4e06fad871f4
PeteDarinzo <birel44@yahoo.com>
2022-04-20 11:20:09 -0400

    modify movies route

--- routes/movies.js
+++ routes/movies.js
@@ -20,7 +20,7 @@ const OMDB_KEY = process.env.OMBD_KEY;
 
 router.get("/title", async (req, res, next) => {
   try {
-    const movies = await axios.get("http://www.omdbapi.com", { params: { apikey: OMDB_KEY }, s: req.query.title });
+    const movies = await axios.get(`${OMDB_URL}`, { params: { s: req.query.title, apikey: OMDB_KEY } });
     return res.status(200).json(movies.data);
   } catch (err) {
     next(err);
